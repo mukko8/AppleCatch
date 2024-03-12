ca7b0c6310d0d75789d068b2e77399ea03db841b
steel <gawct.231020@gmail.com>
2024-03-12 09:52:16 +0900

    UI追加

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -10,6 +10,13 @@ public class ItemGenerator : MonoBehaviour
     float span=1.0f;
     float delta=0;
     int ratio=2;
+    float speed=-0.03f;
+
+    public void SetParameter(float span,float speed,int ratio){
+        this.span=span;
+        this.speed=speed;
+        this.ratio=ratio;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +39,7 @@ public class ItemGenerator : MonoBehaviour
             float x=Random.Range(-1,2);
             float z=Random.Range(-1,2);
             item.transform.position=new Vector3(x,4,z);
+            item.GetComponent<ItemController>().dropSpeed=this.speed;
         }   
     }
 }
